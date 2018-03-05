3aa308d897034afabc204cb0df18969fe776c694
onetcoin <onetcoin2018@gmail.com>
2018-03-05 14:11:02 +0530

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define onetcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "onetcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
